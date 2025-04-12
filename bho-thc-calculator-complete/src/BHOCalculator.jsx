ecba2c29ece80e7d6cea66a1e8e1de0320059231
Bylatdragon <svenclauss195@outlook.de>
2025-04-12 20:33:01 +0200

    Fix: alle relativen Pfade angepasst

--- bho-thc-calculator-complete/src/BHOCalculator.jsx
+++ bho-thc-calculator-complete/src/BHOCalculator.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Input } from "./components/ui/input"
+import { Input } from "../components/ui/input"
 import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
+import { Label } from "../components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Leaf, Info } from "lucide-react";
 
